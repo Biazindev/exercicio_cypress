8f48f998041632669e7787c93b3f68eedb4475b6
Tiago Biazin <tiago.biazin02@gmail.com>
2024-06-12 23:48:53 -0300

    incluindo mochawesome

--- cypress/e2e/agenda-de-contatos/home.cy.js
+++ cypress/e2e/agenda-de-contatos/home.cy.js
@@ -5,13 +5,15 @@ describe('testes para a home', () => {
         cy.visit('https://agenda-contatos-react.vercel.app/')
 
     })
-    
+
     it('Deve incluir nome: Tiago Biazin', () => {
         cy.get('[type="text"]').type('Tiago Biazin')
         cy.get('[type="email"]').type('tiago@testes.com')
         cy.get('[type="tel"]').type('(17)98171-6648')
         cy.get('.adicionar').click()
         cy.get('.sc-iAEyYk > div').should('have.length', 4)
+        cy.screenshot('tela incluindo Cadastro: Tiago Biazin')
+
     })
 
     it('deve alterar o nome para Gian claude van damme', () =>{
@@ -19,9 +21,12 @@ describe('testes para a home', () => {
         cy.get('input[type="text"]').clear()
         cy.get('[type="text"]').type('Gian claude van damme')
         cy.get('button.alterar').first().click()
+        cy.screenshot('tela alterando para Gian claude van damme')
+
     })
 
     it('Deve apagar os segundo nome da lista', () => {
         cy.get('button.delete').eq(1).click();
+        cy.screenshot('tela apagando o segundo da lista')
     })
 })
